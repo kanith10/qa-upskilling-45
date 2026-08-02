2932ef49799f562f459831f11bedac0b355ad5fe
Kanith Aathreyan <aath.kani@gmail.com>
2026-08-02 12:34:39 +0530

    Day 2: Python basics

---
+++ Day02/data_types.py
@@ -0,0 +1,11 @@
+name = "Kanith"
+age = 26
+height = 155.0
+is_qa_engineer = True
+middle_name = None
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_qa_engineer))
+print(type(middle_name))
---
+++ Day02/type_casting.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+height = float(input("Enter your height (in meters): "))
+
+print(type(age))
+print(type(height))
+
+print("Next year your age will be:", age + 1)
+print("Double your height is:", height * 2)
